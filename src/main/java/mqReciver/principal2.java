58fa0de679c51838164e116e254bdd5349682afc
Iñaki Aramburu <56339536+inakiAramburu@users.noreply.github.com>
2023-05-30 10:18:09 +0200

    handleDelivery y cambio pom

--- src/main/java/mqReciver/principal2.java
+++ src/main/java/mqReciver/principal2.java
@@ -52,7 +52,7 @@ public class principal2 {
         }
 
         @Override
-        public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body) {
+        public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
             String message = new String(body, StandardCharsets.UTF_8);
             System.out.println("Mensaje recibido (JSON): " + message);
         }
