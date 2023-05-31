efd386b37e7bcac84b989070624226a7ec3d89f1
Iñaki Aramburu <56339536+inakiAramburu@users.noreply.github.com>
2023-05-31 08:10:59 +0200

    my xd

--- src/main/java/edu/mondragon/soundbeats/IA/main.py
+++ src/main/java/edu/mondragon/soundbeats/IA/main.py
@@ -10,8 +10,7 @@ import librosa.display
 def cargar_mfcc (archivo, duracion=10, sr=22050):
     data = []
     y, sr = librosa.load(archivo, duration=duracion)
-    #mfcc = np.mean(librosa.feature.mfcc(y=y, sr=sr, n_mfcc=25).T,axis=0)
-    mfcc = np.mean(librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40).T,axis=0)
+    mfcc = np.mean(librosa.feature.mfcc(y=y, sr=sr, n_mfcc=25).T,axis=0)
     feature = np.array(mfcc).reshape([-1,1])
     data.append(feature)
     return data
@@ -19,9 +18,6 @@ def cargar_mfcc (archivo, duracion=10, sr=22050):
 archivo = sys.argv[1]
 MODELO = 'SoundBeatsModel.h5'
 
-#MODELO = 'heartbeat_disease.h5'
-
-#model = tf.keras.models.load_model("../" + MODELO)
 model = tf.keras.models.load_model(MODELO)
 
 mfccs = cargar_mfcc(archivo = archivo)
